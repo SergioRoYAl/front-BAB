b784dc33cc4ac17867d63b8bcf6374ec8880807c
Eserome <sergioromeroalarcon2b@gmail.com>
2023-11-13 13:28:52 +0100

    Realizando avances, empezar a crear el menú y mostrar algun dato de la base de datos

---
+++ src/app/model/model.interfaces.ts
@@ -0,0 +1,70 @@
+export interface Sort {
+    empty: boolean;
+    sorted: boolean;
+    unsorted: boolean;
+}
+
+export interface Pageable {
+    sort: Sort;
+    offset: number;
+    pageNumber: number;
+    pageSize: number;
+    paged: boolean;
+    unpaged: boolean;
+}
+
+export interface IPage<T> {
+    content: T[];
+    pageable: Pageable;
+    totalPages: number;
+    totalElements: number;
+    last: boolean;
+    size: number;
+    number: number;
+    sort: Sort;
+    first: boolean;
+    numberOfElements: number;
+    empty: boolean;
+
+    strSortField: string;
+    strSortDirection: string;
+    strFilter: string;
+    strFilteredTitle: string;
+    strFilteredMessage: string;
+    nRecords: number;
+}
+
+export interface IEntity{
+    id: number;
+}
+
+export interface IUser extends IEntity{
+    nombre_empresa: string,
+    username: string,
+    password: string,
+    identificador_empresarial: string
+}
+
+export interface IUserPage extends IPage<IUser>{
+
+}
+
+export interface IPedido extends IEntity{
+    user: IUser;
+    fecha: Date;
+    observaciones: string;
+    establecimiento: IEstablecimiento;
+}
+
+export interface IPedidoPage extends IPage<IPedido>{
+}
+
+export interface IEstablecimiento extends IEntity{
+    pedidos: number;
+    repartidor: string;
+}
+
+export interface IEstablecimientoPage extends IPage<IEstablecimiento>{
+}
+
+
